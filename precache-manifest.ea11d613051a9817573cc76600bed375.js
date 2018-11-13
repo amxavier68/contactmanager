b588769e7ece81116ca07c02460ff46213130f10
T Rex <anthony-xavier@live.com>
2018-11-13 14:13:14 +1100

    Updates

---
+++ precache-manifest.ea11d613051a9817573cc76600bed375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "d064dec71e6dadcd7d45",
+    "url": "/contactmanager/static/js/main.d064dec7.chunk.js"
+  },
+  {
+    "revision": "05dfcb26d7db5d562d57",
+    "url": "/contactmanager/static/js/1.05dfcb26.chunk.js"
+  },
+  {
+    "revision": "d064dec71e6dadcd7d45",
+    "url": "/contactmanager/static/css/main.8dbd552b.chunk.css"
+  },
+  {
+    "revision": "05dfcb26d7db5d562d57",
+    "url": "/contactmanager/static/css/1.12e6c9fc.chunk.css"
+  },
+  {
+    "revision": "dc69741cf7621e1ffe8e7ca76d24ae51",
+    "url": "/contactmanager/index.html"
+  }
+];
